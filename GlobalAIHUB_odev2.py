64aa0fddef0e620cbcf589e2724f8bfb804c5e0b
Bora Eskicioğlu <76914128+Besk61@users.noreply.github.com>
2021-05-27 16:32:33 +0300

    Global AI HUB ödev 2
    Python'a giriş eğitiminde verilmiş olan ikinci ödev...

---
+++ GlobalAIHUB_odev2.py
@@ -0,0 +1,30 @@
+"""Bora Eskicioğlu-Global AI HUB-Python ödev 2"""
+
+cv1={"name":"Bora Eskicioğlu",
+     "Year of birth":"2000",
+     "education":"Recep Tayyip Erdogan University",
+     "department":"Electrical Electronics Engineering"
+}
+cv2 = {"name":"Tamsin Cole",
+     "Year of birth":"1997",
+     "education":"George Washington University",
+     "department":"Medical School"
+}
+cv3 = {"name":"Trent Cresswell",
+     "Year of birth":"2001",
+     "education":"Colgate University",
+     "department":"Faculty of Architecture"
+}
+cv4 = {"name":"Rian Mellor",
+     "Year of birth":"1986",
+     "education":"Franklin and Marshall College",
+     "department":"Civil Engineering"
+}
+cv5 = {"name":"Julian Riddle",
+     "Year of birth":"2002",
+     "education":"Vassar College",
+     "department":"Math Teaching"
+
+}
+
+print(cv1,"\n",cv2,"\n",cv3,"\n",cv4,"\n",cv5)
